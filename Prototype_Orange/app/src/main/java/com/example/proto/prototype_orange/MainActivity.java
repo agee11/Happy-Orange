8b88d3ba041e3de45406ec80fc827432ce6c3693
agee1 <orangemadcow@yahoo.com>
2016-07-09 15:05:01 -0700

    Add image from server feature using FB storage.

--- Prototype_Orange/app/src/main/java/com/example/proto/prototype_orange/MainActivity.java
+++ Prototype_Orange/app/src/main/java/com/example/proto/prototype_orange/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.proto.prototype_orange;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,15 +15,22 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Random;
 import android.view.GestureDetector;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -35,6 +47,7 @@ public class MainActivity extends AppCompatActivity{
     float xOffset;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("Eggroll");
+    StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -57,9 +70,9 @@ public class MainActivity extends AppCompatActivity{
         eggroll.setRestaurant("Panda Express");
         eggroll.setImageId(R.drawable.eggroll);
 
+        storageRef = FirebaseStorage.getInstance().getReferenceFromUrl("gs://happy-orange.appspot.com/");
+
 ///////////////////////////////////////////////////////////////////
-        //myRef.setValue("Whats up");
-        //System.out.println(dataSnapshot.getValue(String.class));
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -80,16 +93,7 @@ public class MainActivity extends AppCompatActivity{
             //System.out.println(rn);
             }
         });
- /*       dishImage.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                if(dishName.getText() == "Egg Roll") {
-                    Intent intent = new Intent(MainActivity.this, DishInfo.class);
-                    intent.putExtra("dish", eggroll);
-                    startActivity(intent);
-                }
-            }
-        });
-        */
+
         dishImage.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -115,9 +119,6 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
-        //dishImage.setOnTouchListener(this);
-
-
         go.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 //if(dishName.getText() == "Egg Roll") {
@@ -132,6 +133,7 @@ public class MainActivity extends AppCompatActivity{
     void passDish(){
         Random rn = new Random();
         int prev;
+
         System.out.println(current);
         do{
             prev = current;
@@ -149,8 +151,46 @@ public class MainActivity extends AppCompatActivity{
                 dishImage.setImageResource(R.drawable.curly_fries);
                 break;
             case 3: dishName.setText("Egg Roll");
-                dishImage.setImageResource(R.drawable.eggroll);
+                //dishImage.setImageResource(R.drawable.eggroll);
+                //Bitmap bmp = BitmapFactory.decodeStream();
+                //dishImage.setImageBitmap(bmp);
+                storageRef.child("eggroll.jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>(){
+
+                    @Override
+                    public void onSuccess(Uri uri){
+                        System.out.println("Storage URI: " + uri);
+                        //URL url = new URL(uri);
+                        DownloadImageTask downloadImageTask = new DownloadImageTask();
+                        downloadImageTask.execute(uri);
+                        //uriToImage(uri);
+                    }
+                });
                 break;
+        }
+    }
+
+    private class DownloadImageTask extends AsyncTask<Uri, Void, Void>{
+        Bitmap bmp;
+
+        @Override
+        protected Void doInBackground(Uri... params) {
+            try {
+                //InputStream imageStream = getContentResolver().openInputStream(uri);
+                URL url = new URL(params[0].toString());
+                System.out.println("Storage URL: " + url);
+                bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                //Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            dishImage.setImageBitmap(bmp);
+            //dishImage.set
         }
     }
 
